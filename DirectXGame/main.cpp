2bffbf7d89743902c29ba2023d5ffaaf09eb665d
GC1A-TinLong <147121460+GC1A-TinLong@users.noreply.github.com>
2024-04-15 16:59:42 +0900

    01_01_確認課題

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2A_08_チョウ_テンロウ_AL3");
+	win->CreateGameWindow(L"GC2A_08_チョウ_テンロウ_AL3");//new title
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
